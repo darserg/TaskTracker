40251f25168e646478d395c6d2ac06074d61e2eb
Darserg <Darserg1212>
2024-02-25 15:03:02 +0400

    Initial Commit

---
+++ TaskTracer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTracer
+//
+//  Created by Сергей Дарьин on 25.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTracer/TaskTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTracerApp.swift
+//  TaskTracer
+//
+//  Created by Сергей Дарьин on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
